afdc79e00463c03b5ca2e26dfd6dde6c07208302
iamsam99 <shksameer603@gmail.com>
2025-06-16 22:35:49 +0530

    Sale And SaleItems Model Added

---
+++ PharmaSuiteWebAPI/Model/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaSuiteWebAPI.Model
+{
+    public class Sale
+    {
+        [Key]
+        public int SaleId { get; set; }
+
+        public DateTime SaleDate { get; set; } = DateTime.Now;
+
+        [MaxLength(200)]
+        public string CustomerName { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public List<SaleItem> SaleItems { get; set; }
+    }
+}
---
+++ PharmaSuiteWebAPI/Model/SaleItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PharmaSuiteWebAPI.Model
+{
+    public class SaleItem
+    {
+        [Key]
+        public int ItemId { get; set; }
+
+        public int SaleId { get; set; }
+
+        [ForeignKey("SaleId")]
+        public Sale Sale { get; set; }
+
+        public int MedicineId { get; set; }
+
+        [ForeignKey("MedicineId")]
+        public Medicine_Management Medicine { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double PricePerUnit { get; set; }
+
+        public double? Discount { get; set; }
+    }
+}
