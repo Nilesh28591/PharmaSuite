fd48c0f1db4288d32daf73b32300c5e0f2b9c3db
iamsam99 <shksameer603@gmail.com>
2025-06-21 20:40:21 +0530

    online payment added

--- PharmaSuiteMVC/Program.cs
+++ PharmaSuiteMVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Sale}/{action=Sale}/{id?}");
 
 app.Run();
